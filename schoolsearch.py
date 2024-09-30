02bd14832db740363c36f03e2489215632deed56
hannah <113932134+hanmosh@users.noreply.github.com>
2024-09-29 22:26:37 -0700

    revert changes to selection len check and trim spaces instead

--- schoolsearch.py
+++ schoolsearch.py
@@ -21,30 +21,30 @@ def _searchAndPrint(filter, studentArray, comparisonIndex, outputIndices):
             print()    
 
 def searchByTeacher(selection, studentArray):
-    if(len(selection) == 1): 
+    if(len(selection) != 2): 
         print("Invalid usage: ", selection)
         return
 
     _searchAndPrint(selection[1],studentArray, TLASTNAME, [LASTNAME,FIRSTNAME])
 
 def searchByBus(selection, studentArray):
-    if(len(selection) == 1): 
+    if(len(selection) != 2): 
         print("Invalid usage: ", selection)
         return
 
     _searchAndPrint(selection[1],studentArray, BUS, [LASTNAME,FIRSTNAME])
 
 def searchByStudent(selection, studentArray):
-    if(len(selection) == 2 or (len(selection) >= 3 and selection[2] != "B" and selection[2] != "Bus")):
+    if(len(selection) == 2):
         _searchAndPrint(selection[1], studentArray, LASTNAME, [LASTNAME,FIRSTNAME, GRADE,
                                                                 CLASSROOM, TLASTNAME, TFIRSTNAME])
-    elif (len(selection) >= 3 and (selection[2] == "B" or selection[2] == "Bus")):
+    elif (len(selection) == 3 and (selection[2] == "B" or selection[2] == "Bus")):
         _searchAndPrint(selection[1], studentArray, LASTNAME, [LASTNAME, FIRSTNAME, BUS])
     else: 
         print("Invalid usage: ", selection)
 
 def average(selection, studentArray):
-    if(len(selection) == 1): 
+    if(len(selection) != 2): 
         print("Invalid usage: ", selection)
         return
 
@@ -81,7 +81,7 @@ def main():
 
     print(infoString)
     while(True):
-        selection = input().split(" ")
+        selection = input().split()
         match selection[0]:
             case "Q" | "Quit":  exit(0)
             case "S:" | "Student:" : searchByStudent(selection, studentArray)
